13fe5b532d291ae7e3bc7ec69f979244b5a2291d
Tatiana Selezneva <tselezne@lxplus969.cern.ch>
2024-10-18 16:21:30 +0200

    Refactor code for improved readability and modularity

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("data.csv")
-plt.plot(
-    np.arange(20),
-    np.poly1d(np.polyfit(data[:, 0], data[:, 1], 1))(np.arange(20)),
-    color="r",
-)
-plt.scatter(data[:, 0], data[:, 1])
-plt.xlabel("x")
-plt.ylabel("y")
-plt.savefig("plot.png")
+def load_data(file_path):
+    return np.loadtxt(file_path)
+
+def plot_data(data, output_file):
+    x = data[:, 0]
+    y = data[:, 1]
+    fit = np.poly1d(np.polyfit(x, y, 1))(np.arange(20))
+    
+    plt.plot(np.arange(20), fit, color="r")
+    plt.scatter(x, y)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.savefig(output_file)
+
+def main():
+    data = load_data("data.csv")
+    plot_data(data, "plot.png")
+
+if __name__ == "__main__":
+    main()
